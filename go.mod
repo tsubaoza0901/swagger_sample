9c40821931fc544cae14bba16706ea67090e11b8
Tsubasa Ozawa <t.ozawa.sake@gmail.com>
2021-01-29 23:49:17 +0900

    swaggerの追加

--- go.mod
+++ go.mod
@@ -3,6 +3,18 @@ module ozawa-swagger-sample
 go 1.15
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/go-openapi/spec v0.20.2 // indirect
 	github.com/labstack/echo/v4 v4.1.17
 	github.com/labstack/gommon v0.3.0
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/swaggo/echo-swagger v1.1.0
+	github.com/swaggo/swag v1.7.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/mod v0.4.1 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+	golang.org/x/tools v0.1.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"net/http"
 	"strconv"
 
+	echoSwagger "github.com/swaggo/echo-swagger"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
+
+	"ozawa-swagger-sample/docs"
 )
 
 // --------
@@ -14,9 +18,9 @@ import (
 
 // User ...
 type User struct {
-	ID   uint   `json:"id" param:"id"`
-	Name string `json:"name" validate:"required"`
-	Age  int    `json:"age"`
+	ID   uint   `json:"id" example:"1"`
+	Name string `json:"name" example:"Tom"`
+	Age  int    `json:"age" example:"20"`
 }
 
 // --------
@@ -76,6 +80,18 @@ func (u *User) DeleteUser(c echo.Context) error {
 }
 
 // GetUser ...
+// @Summary Show a user
+// @Description get user by ID
+// @ID get-user-by-int
+// @Accept application/json
+// @Produce application/json
+// @Param id path int true "User ID"
+// @Success 200 {object} User
+// @Failure 400,404 "Error"
+// @Failure 500 "Error"
+// @Failure default "Error"
+// @Resource /user
+// @Router /user/{id} [get]
 func (u *User) GetUser(c echo.Context) error {
 	user := User{}
 
@@ -158,8 +174,27 @@ func (u *User) GetUsers(c echo.Context) error {
 // main.go↓
 // --------
 
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 func main() {
 	e := echo.New()
+
+	// swagger用のルーティング
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	// programmaticallyにswagger infoを設定したい場合は以下のようにすることも可能
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "This is a sample server."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "localhost:9105"
+	docs.SwaggerInfo.BasePath = "/"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
 	u := new(User)
 	InitRouting(e, u)
